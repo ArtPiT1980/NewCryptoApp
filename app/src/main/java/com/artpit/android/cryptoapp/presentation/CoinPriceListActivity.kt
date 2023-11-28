0ecb3142ca2a94a1377c1810cc6852faae1af058
artpit <artpit.phone@gmail.com>
2023-11-28 18:57:45 +0200

    Refactoring Step 9

--- app/src/main/java/com/artpit/android/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/artpit/android/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.artpit.android.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.artpit.android.cryptoapp.R
 import com.artpit.android.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.artpit.android.cryptoapp.domain.CoinInfo
 import com.artpit.android.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -20,19 +21,38 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val coinDetailInfoIntent = CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(coinDetailInfoIntent)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
-
         }
+
         binding.rvCoinPriceList.adapter = adapter
         binding.rvCoinPriceList.itemAnimator = null
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    private fun isOnePaneMode(): Boolean = (binding.fragmentContainer == null)
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val coinDetailInfoIntent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(coinDetailInfoIntent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
